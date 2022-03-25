59ce9a85bce016a8faa845fd890a5f2fb40caabf
Finn-Lennart Heemeyer <fh@tpwd.de>
2022-03-25 15:22:29 +0100

    Remove unnecessary boilerplate graphql-ruby code

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,7 +1,5 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
-    field_class Types::BaseField
-
     def self.field_with_deprecated_sibling(field_name, type, null:)
       reason = "Snake case fields are deprecated. Please use #{field_name.to_s.camelize(:lower)}."
 
