2eb3886056c1409bc4edc1909010494aacc07be8
Finn Heemeyer <fh@tpwd.de>
2022-04-09 03:22:04 +0200

    Remove comments in BaseObject

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,14 +1,5 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
-    # def self.field(field_name, type, null:)
-    #   super
-
-    #   if field_name.include?("_")
-    #     reason = "Snake case fields are deprecated. Please use #{field_name.to_s.camelize(:lower)}."
-    #     super field_name, type, null: null, camelize: false, deprecation_reason: reason
-    #   end
-    # end
-
     def self.field(*args, **kwargs, &block)
       super(*args, **kwargs, &block)
 
@@ -20,12 +11,5 @@ module Types
         super(*args, **kwargs, &block)
       end
     end
-
-    # def self.field_with_deprecated_sibling(field_name, type, null:)
-    #   reason = "Snake case fields are deprecated. Please use #{field_name.to_s.camelize(:lower)}."
-
-    #   field field_name, type, null: null
-    #   field field_name, type, null: null, camelize: false, deprecation_reason: reason
-    # end
   end
 end
